078a03293fe50a16fa3912067b5c102435769971
Poonyawat Thareepatheep <poonyawatla@gmail.com>
2024-09-09 22:03:43 +0700

    add navbar and sidebar
    add list
    components:
    -navbar
    -sidebar

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
