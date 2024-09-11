f98d1374fb2c7a3d2b80d3829fcf3ddd2d9bc449
B6406325 <milindafook@gmail.com>
2024-09-12 01:26:31 +0700

    create profile layout

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -5,19 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SummaryComponent } from './pages/profile/summary/summary.component';
+import { EditComponent } from './pages/profile/edit/edit.component';
+import { CSummaryComponent } from './components/profile/c-summary/c-summary.component';
+import { CEditComponent } from './components/profile/c-edit/c-edit.component';
+import { CTitleComponent } from './components/profile/c-title/c-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SummaryComponent,
+    EditComponent,
+    CSummaryComponent,
+    CEditComponent,
+    CTitleComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
   ],
   bootstrap: [AppComponent]
 })
---
+++ Project/src/app/components/profile/c-title/c-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-c-title',
+  templateUrl: './c-title.component.html',
+  styleUrl: './c-title.component.css'
+})
+export class CTitleComponent {
+
+}
