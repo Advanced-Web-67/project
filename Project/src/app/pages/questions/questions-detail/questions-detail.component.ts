81440eaad48196381ea5b39d626c1ee9d987c1cb
Poonyawat Thareepatheep <poonyawatla@gmail.com>
2024-09-19 18:11:08 +0700

    re-ask-button

--- Project/src/app/pages/questions/questions-detail/questions-detail.component.ts
+++ Project/src/app/pages/questions/questions-detail/questions-detail.component.ts
@@ -20,11 +20,15 @@ export class QuestionsDetailComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private questionService: QuestionService,
-    private userService: UserdataService
+    private userService: UserdataService,
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
       if (id) {
         this.loadQuestion(id);
@@ -34,22 +38,52 @@ export class QuestionsDetailComponent implements OnInit {
 
   loadQuestion(id: string): void {
     this.questionService.getQuestionById(id).subscribe(
-      question => {
+      (question) => {
         this.question = question;
-        
+
         // หลังจากดึงข้อมูลคำถามแล้ว เรียกข้อมูลผู้ใช้จาก user_id
         this.userService.getUser(question.user_id).subscribe(
-          user => {
+          (user) => {
             this.username = user.username; // เก็บ username ที่ได้จากผู้ใช้
           },
-          error => {
+          (error) => {
             console.error('Error loading user', error);
           }
         );
       },
-      error => {
+      (error) => {
         console.error('Error loading question', error);
       }
     );
+  }
+  onSubmit(): void {
+    if (this.createAnswerForm.valid) {
+      const { answertext, user_id, question_id} = this.createAnswerForm.value;
+      
+      // ตรวจสอบว่า token มีอยู่หรือไม่
+      const token = localStorage.getItem('token');
+      if (!token) {
+        this.toastr.error('กรุณาเข้าสู่ระบบก่อน!', 'Unauthorized');
+        return;
+      }
+
+      // ตั้งค่า header สำหรับการส่ง token ในการ request
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`
+      });
+
+      // ส่งคำถามพร้อมกับรูปภาพในรูปแบบ base64
+      this.http.post('http://localhost:3000/question/answer', { answertext, user_id, question_id}, { headers })
+        .subscribe(
+          (response: any) => {
+            this.toastr.success('สร้างคำถามสำเร็จ!', 'Success');
+            // this.router.navigate(['/questions']); // เปลี่ยนไปที่หน้าหลักของคำถาม
+          },
+          (error) => {
+            this.toastr.error('เกิดข้อผิดพลาดในการสร้างคำถาม', 'Error');
+            console.error('Error:', error);
+          }
+        );
+    }
   }
 }
