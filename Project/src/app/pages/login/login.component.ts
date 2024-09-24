24e7c3b6fbf1c89dbc2798a6b4b4a4125024c8d6
B6406325 <milindafook@gmail.com>
2024-09-24 16:53:20 +0700

    1

--- Project/src/app/pages/login/login.component.ts
+++ Project/src/app/pages/login/login.component.ts
@@ -51,7 +51,7 @@ export class LoginComponent implements OnInit {
 
             setTimeout(() => {
               window.location.reload()
-            }, 3000); 
+            }, 2050); 
 
           },
           (error) => {
